3ef7b1cefe9a9db8cd8f66df25a6cafde270eca4
bimanathanael95@gmai.com <bimanathanael95@gmail.com>
2020-10-09 19:55:16 +0700

    configure heroku deploy

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // const mongo = require('./config/mongo')
+require('dotenv').config()
 const routes = require('./routes')
 const express = require('express')
 const mongoose = require('mongoose');
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT ||3000
 
 //mongoo connection
 mongoose.connect('mongodb://127.0.0.1:27017/DaisiAppDB', { useNewUrlParser: true });

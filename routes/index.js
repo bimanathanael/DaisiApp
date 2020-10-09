f532b288d97a4bcfac0612f66893897c3054bc36
bimanathanael95@gmai.com <bimanathanael95@gmail.com>
2020-10-09 15:31:12 +0700

    fix typo

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ const ProfileControllers = require('../controllers/ProfileControllers')
 router.get('/profile', ProfileControllers.allProfile)
 router.get('/profile/byTags', ProfileControllers.allTagProf)
 router.get('/profile/:phone', ProfileControllers.profByPhone)
+
+//token asal untuk new user
+//wajib untuk existing user
 router.post('/profile/:token', ProfileControllers.updateProf)
 router.delete('/profile/:phone', ProfileControllers.deleteProf)
 
